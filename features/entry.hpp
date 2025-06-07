6cfdd53efd3ab544710927d76252ab1bab94f267
Caluna123 <Caluna123@users.noreply.github.com>
2025-06-07 17:30:41 -0300

    Update dependencies

--- features/entry.hpp
+++ features/entry.hpp
@@ -12,3 +12,4 @@
 void mainLoop(bool state, MemoryManagement::moduleData client);
 C_CSPlayerPawn doPreferred(C_CSPlayerPawn C_CSPlayerPawn_, CGameSceneNode CGameSceneNode, LocalPlayer localPlayer, uintptr_t preferredTarget, view_matrix_t viewMatrix, int mode, MemoryManagement::moduleData client);
 
+
